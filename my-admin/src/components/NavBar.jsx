e3b506178ae6d1c18f52b58c3781754e823d9fcf
salehjulie <122396421+salehjulie@users.noreply.github.com>
2023-10-19 01:00:22 +0000

    homepage

--- my-admin/src/components/NavBar.jsx
+++ my-admin/src/components/NavBar.jsx
@@ -3,32 +3,31 @@ import React from "react"
 function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg fixed-top">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">Stel Stay</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav mx-auto">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Contact Us</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Apply Now</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-              </ul>
-            </div>
+      <nav className="navbar navbar-expand-lg fixed-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="index.html">
+            <img src="img/stelstay.jpgs" alt="" /> Stel Stay
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="index.html">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="contact.html">Contact Us</a>
+              </li>
+              <li className="nav-item">
+                <button className="btn btn-primary nav-link" href="apply.html">Apply Now</button>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
-      )
+        </div>
+      </nav>
+    </div>
+  )
 }
 
-      export default Navbar
+export default Navbar
